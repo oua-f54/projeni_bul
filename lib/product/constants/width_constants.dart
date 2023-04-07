ab4c4e1fcc4231ea4fbbf051b246bd94c2e0f1c3
Erhan Gocen <71042486+erhangocen@users.noreply.github.com>
2023-04-07 15:20:25 +0300

    .

--- lib/product/constants/width_constants.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class WidthConstants{
-  static double onboardItmeSize(context) => MediaQuery.of(context).size.width * 9/10;
-}
--- lib/product/views/onboard/onboard_view_model.dart
+++ lib/product/views/onboard/onboard_view_model.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:jam_architecture/app/app_base_view_model.dart';
 import 'package:jam_architecture/product/views/onboard/onboard_view.dart';
 
-import '../../models/onboard_model.dart';
+import '../../constants/image_constants_enum.dart';
+import '../../constants/text_constants.dart';
 
 abstract class OnboardViewModel extends BaseViewModel<OnboardView>{
   int currentIndex = 0;
@@ -52,4 +53,22 @@ abstract class OnboardViewModel extends BaseViewModel<OnboardView>{
     }
     isBackEnable.value = value;
   }
+}
+
+class Onboard{
+  final String title;
+  final String description;
+  final String imagePath;
+
+  Onboard(this.title, this.description, this.imagePath);
+
+  String get imageWithPath => "asset/images/$imagePath.png";
+}
+
+class OnboardItems {
+  static final List<Onboard> datas = [
+    Onboard(TextConstants.onboardTitle, TextConstants.onboardSubtitle, ImageConstants.icChef.toName),
+    Onboard(TextConstants.onboardTitle, TextConstants.onboardSubtitle, ImageConstants.icDelivery.toName),
+    Onboard(TextConstants.onboardTitle, TextConstants.onboardSubtitle, ImageConstants.icOrder.toName)
+  ];
 }
