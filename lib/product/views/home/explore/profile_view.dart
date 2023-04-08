77407fd117bfe9eed797b6c957dcebd36957911b
MELİH <94020867+mrxmelih@users.noreply.github.com>
2023-04-08 23:19:50 +0300

    refactoring

--- lib/product/views/home/explore/profile_view.dart
+++ lib/product/views/home/explore/profile_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jam_architecture/product/constants/edgeInsents_constants.dart';
 import 'package:jam_architecture/product/constants/image_constants_enum.dart';
 import 'package:jam_architecture/repositories/user_repository.dart';
 import 'package:kartal/kartal.dart';
@@ -12,69 +13,74 @@ class ProfileView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: context.width,
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 15),
+        body: SingleChildScrollView(
+      child: SizedBox(
+        width: context.width,
+        child: Column(
+          children: [
+            Container(
+                padding: EdgeInsetsConstants.exploreSizeBoxPadding,
                 width: context.dynamicWidth(0.5),
                 child: TextButton(
-                  onPressed: (){},
-                  child: Image.asset(ImageConstants.defaultPp.toPath)
-                )
-              ),
-              Container(
-                child: Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(3),
-                      child: Text(
-                        "Ahmet", 
-                      style: context.textTheme.titleLarge,),
+                    onPressed: () {},
+                    child: Image.asset(ImageConstants.defaultPp.toPath))),
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(3),
+                    child: Text(
+                      "Ahmet",
+                      style: context.textTheme.titleLarge,
                     ),
-                    Container(
-                      padding: const EdgeInsets.all(3),
-                      child: Text(ref.read(userRepositoryNotifer).auth.currentUser?.email??"", style: context.textTheme.bodySmall?.copyWith(fontSize: 15),),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical:15),
-                child: Column(
-                  children: [
-                    SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Container(
-                  padding: const EdgeInsets.all(15),
-                  child: Text("Your Projects", style: context.textTheme.headlineSmall?.copyWith(
-                    fontWeight: FontWeight.w800
-                  ),),
-                ),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Container(
-                  padding: const EdgeInsets.only(bottom: 50),
-                  child: Row(
-                    children: const [
-                      ProjectCard(),
-                      ProjectCard(),
-                      ProjectCard(),
-                      ProjectCard(),
-                    ],
                   ),
-                ),
+                  Container(
+                    padding: EdgeInsetsConstants.textEmailPadding,
+                    child: Text(
+                      ref.read(userRepositoryNotifer).auth.currentUser?.email ??
+                          "",
+                      style:
+                          context.textTheme.bodySmall?.copyWith(fontSize: 15),
+                    ),
+                  ),
+                ],
               ),
-                  ],
-                ),
-              )
-            ],
-          ),
+            ),
+            Container(
+              padding: EdgeInsetsConstants.exploreTittlePadding,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Container(
+                      padding: EdgeInsetsConstants.textProjectPadding,
+                      child: Text(
+                        "Your Projects",
+                        style: context.textTheme.headlineSmall
+                            ?.copyWith(fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Container(
+                      padding: EdgeInsetsConstants.rowBottomPadding,
+                      child: Row(
+                        children: const [
+                          ProjectCard(),
+                          ProjectCard(),
+                          ProjectCard(),
+                          ProjectCard(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
-      )
-    );
+      ),
+    ));
   }
 }
